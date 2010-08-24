3b0d71dd26be23845c6055188490b6fa1b6e43a1
Siim Meerits <sh0@altamira.eu>
2010-08-24 16:22:13 +0300

    Added more comments to injection code.

--- src/pk_inject_tcp.c
+++ src/pk_inject_tcp.c
@@ -27,7 +27,7 @@
 static void inj_tcp_raw(st_ao_packet* pck, guint8* rsp_data, guint32 rsp_len, guint8 tcp_flags, guint32* tcp_seq);
 
 /**
- * \brief Inject TCP packet
+ * \brief Inject data into TCP stream.
  * \param pck Received TCP packet that will be replied
  * \param pl_data Payload data
  * |param pl_size Payload size
@@ -40,43 +40,50 @@ void inj_tcp(st_ao_packet* pck, guint8* pl_data, guint32 pl_size)
     // Sequence
     guint32 tcp_seq = ntohl(pck->m4.tcp.hdr->ack_seq);
     
-    //usleep(1000 * 10);
-    
-    // Fragment
+    // Device MTU
     guint32 mtu = 1000;
+    
+    // Fragment - if payload is bigger than MTU then send in multiple chunks
     guint32 offset;
     for (offset = 0; offset < pl_size; offset += mtu){
         guint16 len = pl_size - offset;
         if(len > mtu)
             len = mtu;
-
-        //ao_inj_temp(pck->lor_ctx, pck->m2.dot11.iw, pck->m2.dot11.llc, pck->m3.ipv4.hdr, pck->m4.tcp.hdr, (char*)(pl_data + offset), len, TH_PUSH | TH_ACK, &tcp_seq);
-        //ao_inj_tcp_raw(pck, NULL, 0, TH_ACK, &tcp_seq);
-        inj_tcp_raw(pck, pl_data + offset, len, TH_PUSH | TH_ACK, &tcp_seq); // 
+        inj_tcp_raw(pck, pl_data + offset, len, TH_PUSH | TH_ACK, &tcp_seq);
     }
 
-    // Connection reset
-    if (0)
-        //ao_inj_temp(pck->lor_ctx, pck->m2.dot11.iw, pck->m2.dot11.llc, pck->m3.ipv4.hdr, pck->m4.tcp.hdr, NULL, 0, TH_RST | TH_ACK, &tcp_seq);
-        inj_tcp_raw(pck, NULL, 0, TH_RST | TH_ACK, &tcp_seq);
+    // Connection reset - terminates the connection. If this is not done
+    // then the client will see our payload followed by the real data from net.
+    //inj_tcp_raw(pck, NULL, 0, TH_RST | TH_ACK, &tcp_seq);
 }
 
+/**
+ * \brief Injects one TCP packet into the stream.
+ * \param pck Received TCP packet that will be replied
+ * \param rsp_data Payload data (can be NULL)
+ * \param rsp_len Payload size
+ * \param tcp_flags TCP packet flags (TH_SYN, TH_ACK, TH_PUSH, TH_FIN, TH_RST)
+ * \param tcp_seq TCP ACK sequence number; it is automatically updated
+ */
 static void inj_tcp_raw(st_ao_packet* pck, guint8* rsp_data, guint32 rsp_len, guint8 tcp_flags, guint32* tcp_seq)
 {
     // Debug
     printf("[inj] sending! len=%u\n", rsp_len);
 
-    // libnet wants the data in host-byte-order
+    // Libnet wants the data in host-byte-order
     u_int tcp_ack = ntohl(pck->m4.tcp.hdr->seq) + (ntohs(pck->m3.ipv4.hdr->ip_len) - pck->m3.ipv4.hdr->ip_hl * 4 - pck->m4.tcp.hdr->doff * 4);
 
-    // Timestamps
+    // Timestamps - sometimes timestamps are added to the TCP packets to check
+    // ping times. We may respond also with timestamp added to our payload
+    // packet. This is not required by standards AFAIK and probably can be
+    // omitted.
+    guint32 time_off = 0;
     guint8 time_data[12] = {
         0x01, 0x01,
         0x08, 0x0a,
         0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00
     };
-    guint32 time_off = 0;
     if (pck->m4.tcp.ts != NULL) {
         time_off = 12;
         guint32 time_b = ntohl(pck->m4.tcp.ts->time_a);
@@ -95,7 +102,7 @@ static void inj_tcp_raw(st_ao_packet* pck, guint8* rsp_data, guint32 rsp_len, gu
         }
     }
     
-    // TCP
+    // Build TCP header
     pck->ao_inst->ln_tcp_t = libnet_build_tcp(
         ntohs(pck->m4.tcp.hdr->dest), // source port
         ntohs(pck->m4.tcp.hdr->source), // dest port
@@ -111,12 +118,12 @@ static void inj_tcp_raw(st_ao_packet* pck, guint8* rsp_data, guint32 rsp_len, gu
         pck->ao_inst->ln_inst, // libnet_t pointer
         0 //pck->ao_inst->ln_thd_t //pck->ao_inst->ln_tcp_t // ptag
     );
-
     if (pck->ao_inst->ln_tcp_t == -1){
         g_print("[inj] libnet_build_tcp returns error: %s\n", libnet_geterror(pck->ao_inst->ln_inst));
         return;
     }
 
+    // Build IPv4 header
     pck->ao_inst->ln_ip_t = libnet_build_ipv4(
         40 + rsp_len + time_off, // length
         0, // TOS bits
@@ -132,38 +139,42 @@ static void inj_tcp_raw(st_ao_packet* pck, guint8* rsp_data, guint32 rsp_len, gu
         pck->ao_inst->ln_inst, // libnet_t pointer
         0 //pck->ao_inst->ln_ip_t // ptag
     );
-
     if(pck->ao_inst->ln_ip_t == -1){
         g_print("[inj] libnet_build_ipv4 returns error: %s\n", libnet_geterror(pck->ao_inst->ln_inst));
         return;
     }
 
-    // copy the libnet packets to to a buffer to send raw..
+    // New packet buffer
     uint8_t pck_buf[0x10000];
+    
+    // Copy IEEE80211 header
     struct ieee80211_hdr* hdr_w_n = (struct ieee80211_hdr*) pck_buf;
     memcpy(hdr_w_n, pck->m2.dot11.iw, sizeof(struct ieee80211_hdr));
     
+    // Copy LLC header
     struct llc_hdr* hdr_llc_n = (struct llc_hdr*) (pck_buf + sizeof(struct ieee80211_hdr));
     memcpy(hdr_llc_n, pck->m2.dot11.llc, sizeof(struct llc_hdr));
-
-    // set the FROM_DS flag and swap MAC addresses
+    
+    // Swap FROM_DS and TO_DS flags
     hdr_w_n->u1.fc.from_ds = 1;
     hdr_w_n->u1.fc.to_ds = 0;
+    // Send only data, no QOS headers
     hdr_w_n->u1.fc.subtype = WLAN_FC_SUBTYPE_DATA;
     hdr_w_n->duration = 0x013a;
-    /*
-    if(wepkey)
-        n_w_hdr->flags |= IEEE80211_WEP_FLAG;
-    */
+    // Target is IPv4
     hdr_llc_n->type = LLC_TYPE_IPV4;
-
+    // Swap MAC addresses
     uint8_t tmp_addr[6];
     memcpy(tmp_addr, hdr_w_n->addr1, 6);
     memcpy(hdr_w_n->addr1, hdr_w_n->addr2, 6);
     memcpy(hdr_w_n->addr2, tmp_addr, 6);
-
+    // If WEP encrypted
+    //if (wepkey)
+    //    n_w_hdr->flags |= IEEE80211_WEP_FLAG;
+    
+    // Packet size and libnet buffer
     u_int32_t pck_len;
-    u_int8_t* lnet_pck_buf;
+    u_int8_t* lnet_pck_buf = NULL;
 
     // cull_packet will dump the packet (with correct checksums) into a
     // buffer for us to send via the raw socket
@@ -173,15 +184,17 @@ static void inj_tcp_raw(st_ao_packet* pck, guint8* rsp_data, guint32 rsp_len, gu
         return;
     }
 
+    // Copy IPv4+TCP into buffer
     memcpy(pck_buf + sizeof(struct ieee80211_hdr) + sizeof(struct llc_hdr), lnet_pck_buf, pck_len);
 
+    // Free libnet paket
     libnet_adv_free_packet(pck->ao_inst->ln_inst, lnet_pck_buf);
 
-    // total packet length
+    // Total packet length
     gint len = sizeof(struct ieee80211_hdr) + sizeof(struct llc_hdr) + 40 + time_off + rsp_len;
   
     /*
-    if(wepkey){
+    if (wepkey) {
         uint8_t tmpbuf[0x10000];
         // encryption starts after the 802.11 header, but the LLC header
         // gets encrypted.
@@ -199,7 +212,7 @@ static void inj_tcp_raw(st_ao_packet* pck, guint8* rsp_data, guint32 rsp_len, gu
     */
 
     // Debug
-    dumphex((uint8_t*) pck_buf, len);
+    //dumphex((uint8_t*) pck_buf, len);
     
     // Packet
     /*
